cd2eb379ad28d4e921e35f57dbea0f344a5768a2
Renzo Tissoni <r.tissoni@gmail.com>
2020-04-22 01:58:13 -0300

    Initial Commit

---
+++ Weather Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather Demo
+//
+//  Created by eagerworks 2 on 4/22/20.
+//  Copyright © 2020 eagerWorks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
